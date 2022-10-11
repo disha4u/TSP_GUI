122cff3c442fcb55c663c786dcc04d139a19a046
ramachandrasai7 <40018936+ramachandrasai7@users.noreply.github.com>
2022-10-10 17:59:07 -0700

    GUI Changed

--- GUI.java
+++ GUI.java
@@ -1,4 +1,14 @@
 
 public class GUI {
-
+	
+	public void PlotPoints(City[] Cities)
+	{
+		Points points = new Points();
+		JFrame frame = new JFrame("Points");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(points);
+		frame.setSize(250, 200);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
 }
