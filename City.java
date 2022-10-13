b53d8d668a9598398dc1cbcef1009555e691ff61
disha4u <disha.97.agarwal@gmail.com>
2022-10-12 19:57:20 -0700

    javadoc for city class

--- City.java
+++ City.java
@@ -1,4 +1,8 @@
-
+/*@author Ramachandra Sai
+ *@author Disha Agarawal
+ *@author Harshit 
+ *@version 1
+ */
 public class City {
 
 	private double xcoordinate;
@@ -9,15 +13,30 @@ public class City {
 		ycoordinate=y;
 	}
 	
+	/*
+	 * @param value of x coordinate
+	 */
 	public void setx(double val) {
 		xcoordinate=val;
 	}
+	
+	/*
+	 * @param value of y coordinate
+	 */
 	public void sety(double val) {
 		ycoordinate=val;
 	}
+	
+	/*
+	 * @return value of x coordinate
+	 */
 	public double getx() {
 		return xcoordinate;
 	}
+	
+	/*
+	 * @return value of y coordinate
+	 */
 	public double gety() {
 		return ycoordinate;
 	}
