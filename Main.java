b380aad018e5c71502988269f474d0d56e68dd2c
disha4u <disha.97.agarwal@gmail.com>
2022-10-12 18:58:21 -0700

    removed comments and implemented clean coding in main

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class Main {
+	
    public static void main(String[] args) {
 	   DataFetch df=new DataFetch();
 	   String fpath="Data/Asymmetric_Data/rbg017.2.tw.txt";
-	   Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+	   Scanner myObj = new Scanner(System.in); 
 	   System.out.println("Enter File Path");
 	   fpath=myObj.nextLine();
 	   
@@ -17,19 +17,16 @@ public class Main {
 	   GUI gui=new GUI();
 	   if (fpath.contains("Symmetric")) {
 		   SymmetricDataParse dp= new SymmetricDataParse();
-	   Graph g=dp.dataParse(data);
-	   path=g.caluculateTSP();
-	   ArrayList<City> cities=dp.dataCities(data);
-	   //System.out.println(path);
-	  
-	   gui.plotPoints(cities);
-	   }
-	   else {
-		   AsymmetricDataParse dp= new AsymmetricDataParse();
 		   Graph g=dp.dataParse(data);
 		   path=g.caluculateTSP();
+		   ArrayList<City> cities=dp.dataCities(data);
+		   gui.plotPoints(cities);
 	   }
-	   //System.out.println(path);
+	   else {
+		   AsymmetricDataParse adp= new AsymmetricDataParse();
+		   Graph g=adp.dataParse(data);
+		   path=g.caluculateTSP();
+	   }
 	   gui.displayPath(path);
    }
 }
